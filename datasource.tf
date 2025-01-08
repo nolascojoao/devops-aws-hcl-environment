68184bbe90a61cbd3db91ada1c3413b83df22cac
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-08 19:16:21 -0300

    Add data source to fetch the latest Amazon Linux 2023 AMI

--- datasource.tf
+++ datasource.tf
@@ -1,9 +1,15 @@
+# Data source para obter a AMI mais recente do Amazon Linux 2023
+# O filtro busca pela AMI com o nome que segue o padrão "al2023-ami-2023.6.20241212.0-kernel-*"
+# O parâmetro 'owners' limita a busca às AMIs criadas pela AWS (ID do proprietário: 137112412989)
+# O campo 'most_recent' garante que será retornada a versão mais recente da AMI que corresponde aos critérios.
+# Isso assegura que a última versão disponível do Amazon Linux 2023 será utilizada.
+
 data "aws_ami" "server_ami" {
     most_recent = true
     owners = ["137112412989"]
 
     filter {
-      name = "name"
+      name   = "name"
       values = ["al2023-ami-2023.6.20241212.0-kernel-*"]
     }
 }
