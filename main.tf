439e0d90391575974d5753d3c2212e5087321937
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-08 16:09:05 -0300

    Add public subnet to VPC

--- main.tf
+++ main.tf
@@ -7,3 +7,14 @@ resource "aws_vpc" "vpc-01" {
     Name = "devops"
   }
 }
+
+resource "aws_subnet" "public-subnet-01" {
+    vpc_id = aws_vpc.vpc-01.id
+    cidr_block = "10.0.1.0/24"
+    map_public_ip_on_launch = true
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "devops - public"
+    }
+}
