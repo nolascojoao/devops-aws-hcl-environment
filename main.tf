2f205e15da64c59873cc30a768daa044582d3aba
João Pedro Nolasco <nolascojoaops@gmail.com>
2025-01-08 17:28:15 -0300

    Associate public subnet with public route table

--- main.tf
+++ main.tf
@@ -1,42 +1,47 @@
 resource "aws_vpc" "vpc-01" {
-    cidr_block = "10.0.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
 
-    tags = {
-        Name = "devops"
-    }
+  tags = {
+    Name = "devops"
+  }
 }
 
 resource "aws_subnet" "public-subnet-01" {
-    vpc_id = aws_vpc.vpc-01.id
-    cidr_block = "10.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
+  vpc_id                  = aws_vpc.vpc-01.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-east-1a"
 
-    tags = {
-        Name = "devops-public"
-    }
+  tags = {
+    Name = "devops-public"
+  }
 }
 
 resource "aws_internet_gateway" "IGW-01" {
-    vpc_id = aws_vpc.vpc-01.id
+  vpc_id = aws_vpc.vpc-01.id
 
-    tags = {
-        Name = "devops-igw"
-    }
+  tags = {
+    Name = "devops-igw"
+  }
 }
 
 resource "aws_route_table" "public-rt-01" {
-    vpc_id = aws_vpc.vpc-01.id
+  vpc_id = aws_vpc.vpc-01.id
 
-    tags = {
-        Name = "devops-public-rt-01"
-    }
+  tags = {
+    Name = "devops-public-rt-01"
+  }
 }
 
 resource "aws_route" "default_route" {
-    route_table_id = aws_route_table.public-rt-01.id
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.IGW-01.id
+  route_table_id         = aws_route_table.public-rt-01.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.IGW-01.id
+}
+
+resource "aws_route_table_association" "public-subnet-01-assoc" {
+  subnet_id      = aws_subnet.public-subnet-01.id
+  route_table_id = aws_route_table.public-rt-01.id
 }
